3343b66a5a2584f13652a40212c786d4ce7fd6ab
Jack <jackov@126.com>
2015-06-16 23:10:21 +0800

    推荐分类, 家里DNS解析出问题?

--- src/app/recommend/recommend.controller.js
+++ src/app/recommend/recommend.controller.js
@@ -8,17 +8,30 @@
   // 考虑多种推荐问题, 不采用广播的方式绑定数据, 而从承诺中直接获取
   LeftRecommendCtrl.$inject = ['$scope', 'recommendService'];
   function LeftRecommendCtrl($scope, recommendService) {
-    var _feature = null;
     var that = this;
-    that.title = '相关推荐';
+    that.type = null;
+    that.title = '指标推荐'; // 可以根据类型判读更改
+    that.recommends = null;
 
     // 当前属性是根据控制器上下文获取的, 耦合较高, 为了在多个推荐的情况下可以区分
     if ($scope.dim && $scope.dim.feature) {
-      _feature = $scope.dim.feature;
+      var feature = $scope.dim.feature;
+      that.type = recommendService.getType(feature);
+    } else {
+      console.error('推荐控制上下文特征不存在');
     }
 
-    console.info($scope.dim);
-    recommendService.require();
+    // 获取推荐封装
+    function getRecommend(type) {
+      // 将特征交于服务处理, 判断获取什么样的推荐(现在不需要)
+      recommendService.require(type)
+        .then(function(recommends) {
+console.info('更新推荐:', recommends);
+          that.recommends = recommends;
+        });
+    }
+
+    getRecommend(that.type);
   }
 
 })();
--- src/app/recommend/recommend.service.js
+++ src/app/recommend/recommend.service.js
@@ -5,21 +5,59 @@
     .module('platform.recommend')
     .factory('recommendService', recommendService);
 
-  recommendService.$inject = ['sheetService', 'dataService']
-  function recommendService(sheetService, dataService) {
+  recommendService.$inject = ['sheetService', 'recommendBean', 'dataService']
+  function recommendService(sheetService, recommendBean, dataService) {
+    // 每一组推荐都会根据当前的表以及类型存放在这里, 条件用
+    var _recommendChange = {};
     var service = {
-      'require': getRecomment
+      'require': getRecommend,
+      'getType': getRecommendType
     };
     return service;
 
-    function getRecomment() {
+    function getRecommend(type) {
       var params = {};
       params.sheetId = sheetService.getSheetId();
+      // 根据type可以获取不同的推荐
 
-      dataService.get('recommend', params)
+      return dataService.get('recommend', params)
         .then(function(source) {
-  console.info(source);
+          if (source.length) {
+            var recommends = [];
+            angular.forEach(source, function(recommendSource, index) {
+              var recommend = recommendBean.parse(recommendSource, type);
+              recommends.push(recommend);
+            });
+            setRecommendChange(params.sheetId, type, recommends);
+            console.info(_recommendChange);
+            return recommends;
+          }
+          return $q.reject('不识别的recommend源数据!' + source);
         });
+    }
+
+    /**
+     * 根据维度的特征得到需要的推荐类型
+     * @param  {Object} feature 特性, 由条件模块抽取
+     * @return {String} 一种类型
+     */
+    function getRecommendType(feature) {
+      var type = null;
+      if (feature.indicator === true) {
+        type = 'indicator';
+      }
+      return type;
+    }
+
+    /**
+     * 缓存推荐信息, 以当前表以及推荐的类型区分
+     * @param {[type]} sheetId 当前表的id
+     * @param {[type]} type 推荐的类型
+     */
+    function setRecommendChange(sheetId, type, recommends) {
+      if (!_recommendChange[sheetId]) { _recommendChange[sheetId] = {}; }
+      var sheetRecmd = _recommendChange[sheetId];
+      sheetRecmd[type] = recommends;
     }
   }
 
