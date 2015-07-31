015686ee833a5cd653c2f955b4c35749219d818a
Jack <jackov@126.com>
2015-07-31 17:24:55 +0800

    收拾屋子去

---
+++ src/app/condition/condition.service.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pf.condition')
+    .factory('conditionService', conditionService);
+
+  conditionService.$inject = ['$rootScope'];
+  function conditionService($rootScope) {
+    var service = {};
+    return service;
+
+    
+  }
+
+})();
